a64fa99b037568f2c8a211d2891a3689b4baccff
Joe Shindelar <eojthebrave@gmail.com>
2019-01-02 13:18:42 -0600

    Use drupal_id instead of UUID when creating lists.

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,7 +66,7 @@ export const query = graphql`
     allNodeRecipe(sort: {fields: [changed], order:DESC}) {
       edges {
         node {
-          uuid,
+          drupal_id,
           title,
           path {
             alias,
