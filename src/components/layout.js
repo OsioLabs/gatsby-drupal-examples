17b8941d0b3239acefa43b6174f62abff2a42916
Joe Shindelar <eojthebrave@gmail.com>
2020-11-02 11:22:23 -0600

    Remove unused styles variable.

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,19 +16,6 @@ const drupalOauthClient = new drupalOauth({
   client_secret: 'gatsby',
 });
 
-const styles = theme => ({
-  root: {
-    width: 'auto',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-      width: 1100,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-});
-
 const Layout = (props) => {
   const {children} = props;
 
